3d9ca392d802fe7da45b66d133ca1bf1e6446384
nranthony <nranthony@users.noreply.github.com>
2024-08-05 07:29:44 -0400

    initial setup steps - py3.10

--- experiment-doublepend/data copy.py
+++ experiment-doublepend/data copy.py
@@ -3,66 +3,76 @@
 # Extension to double pendulum | 2024
 # Neil Anthony
 
-import jax
-import jax.numpy as jnp
+import autograd
+import autograd.numpy as np
 
 import scipy.integrate
 solve_ivp = scipy.integrate.solve_ivp
 
+# import numpy as np
+
 def hamiltonian_fn(coords, m1=1, m2=1, l1=1, l2=1, g=3):
-    q, p = jnp.split(coords, 2)
+    q, p = np.split(coords, 2)
     theta1, theta2 = q
     p_theta1, p_theta2 = p
 
-    denominator = 2 * m2 * l1**2 * l2**2 * (m1 + m2 * jnp.sin(theta1 - theta2)**2)
+    denominator = 2 * m2 * l1**2 * l2**2 * (m1 + m2 * np.sin(theta1 - theta2)**2)
     kinetic_energy = (
         m2 * l1**2 * p_theta1**2 +
         (m1 + m2) * l1**2 * l2**2 * p_theta2**2 -
-        2 * m2 * l1 * l2 * p_theta1 * p_theta2 * jnp.cos(theta1 - theta2)
+        2 * m2 * l1 * l2 * p_theta1 * p_theta2 * np.cos(theta1 - theta2)
     ) / denominator
     
     potential_energy = (
-        -(m1 + m2) * g * l1 * jnp.cos(theta1) -
-        m2 * g * l2 * jnp.cos(theta2)
+        -(m1 + m2) * g * l1 * np.cos(theta1) -
+        m2 * g * l2 * np.cos(theta2)
     )
     
     H = kinetic_energy + potential_energy
     return H
 
 def dynamics_fn(t, coords):
-    dcoords = jax.grad(hamiltonian_fn)(coords)
-    dqdt, dpdt = jnp.split(dcoords, 2)
-    S = jnp.concatenate([dpdt, -dqdt], axis=-1)
+    dcoords = autograd.grad(hamiltonian_fn)(coords)
+    dqdt, dpdt = np.split(dcoords,2)
+    S = np.concatenate([dpdt, -dqdt], axis=-1)
     return S
 
-def get_trajectory(t_span=[0, 3], timescale=15, radius=None, y0=None, noise_std=0.1, **kwargs):
-    t_eval = jnp.linspace(t_span[0], t_span[1], int(timescale * (t_span[1] - t_span[0])))
+def get_trajectory(t_span=[0,3], timescale=15, radius=None, y0=None, noise_std=0.1, **kwargs):
+    t_eval = np.linspace(t_span[0], t_span[1], int(timescale*(t_span[1]-t_span[0])))
     
-    y0 = [jnp.pi / 4, jnp.pi / 4, 0.0, 0.0]
+    # get initial state
+    # if y0 is None:
+    #     y0 = np.random.rand(2)*2.-1
+    # if radius is None:
+    #     radius = np.random.rand() + 1.3 # sample a range of radii
+    # y0 = y0 / np.sqrt((y0**2).sum()) * radius ## set the appropriate radius
+    y0 = [np.pi / 4, np.pi / 4, 0.0, 0.0]
+
 
     doublepend_ivp = solve_ivp(fun=dynamics_fn, t_span=t_span, y0=y0, t_eval=t_eval, rtol=1e-10, **kwargs)
     q, p = doublepend_ivp['y'][0], doublepend_ivp['y'][1]
     dydt = [dynamics_fn(None, y) for y in doublepend_ivp['y'].T]
-    dydt = jnp.stack(dydt).T
-    dqdt, dpdt = jnp.split(dydt, 2)
+    dydt = np.stack(dydt).T
+    dqdt, dpdt = np.split(dydt,2)
     
     # add noise
-    q += jnp.random.randn(*q.shape) * noise_std
-    p += jnp.random.randn(*p.shape) * noise_std
+    q += np.random.randn(*q.shape)*noise_std
+    p += np.random.randn(*p.shape)*noise_std
     return q, p, dqdt, dpdt, t_eval
 
 def get_dataset(seed=0, samples=50, test_split=0.5, **kwargs):
     data = {'meta': locals()}
 
-    jax.random.PRNGKey(seed)
+    # randomly sample inputs
+    np.random.seed(seed)
     xs, dxs = [], []
     for s in range(samples):
         x, y, dx, dy, t = get_trajectory(**kwargs)
-        xs.append(jnp.stack([x, y]).T)
-        dxs.append(jnp.stack([dx, dy]).T)
+        xs.append( np.stack( [x, y]).T )
+        dxs.append( np.stack( [dx, dy]).T )
         
-    data['x'] = jnp.concatenate(xs)
-    data['dx'] = jnp.concatenate(dxs).squeeze()
+    data['x'] = np.concatenate(xs)
+    data['dx'] = np.concatenate(dxs).squeeze()
 
     # make a train/test split
     split_ix = int(len(data['x']) * test_split)
@@ -76,12 +86,12 @@ def get_field(xmin=-1.2, xmax=1.2, ymin=-1.2, ymax=1.2, gridsize=20):
     field = {'meta': locals()}
 
     # meshgrid to get vector field
-    b, a = jnp.meshgrid(jnp.linspace(xmin, xmax, gridsize), jnp.linspace(ymin, ymax, gridsize))
-    ys = jnp.stack([b.flatten(), a.flatten()])
+    b, a = np.meshgrid(np.linspace(xmin, xmax, gridsize), np.linspace(ymin, ymax, gridsize))
+    ys = np.stack([b.flatten(), a.flatten()])
     
     # get vector directions
     dydt = [dynamics_fn(None, y) for y in ys.T]
-    dydt = jnp.stack(dydt).T
+    dydt = np.stack(dydt).T
 
     field['x'] = ys.T
     field['dx'] = dydt.T
